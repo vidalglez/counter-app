47b7317fe05465bc9ba1a66d6c8e26c4fb4bde53
Vidal Gonzalez <darkvidal@gmail.com>
2018-11-21 21:32:51 -0600

    feature: asynchronous call for puzzle word

--- src/playground/hangman/app.js
+++ src/playground/hangman/app.js
@@ -1,4 +1,4 @@
-const h1 = new Hangman('Cat', 2);
+const h1 = new Hangman('Car play', 2);
 document.querySelector("#status").textContent = h1.statusMessage
 
 window.addEventListener('keypress', e => {
@@ -15,3 +15,18 @@ const spanElement = () => {
 };
 
 document.querySelector('#puzzle').textContent = h1.puzzle;
+
+const request = new XMLHttpRequest()
+
+request.addEventListener('readystatechange', (e) => {
+    if(e.target.readyState === 4 && e.target.status === 200) {
+        const data = JSON.parse(e.target.responseText)
+        console.log(data)
+    } else if(e.target.readyState === 4){
+        console.log('An error has ocurred')
+    }
+    
+})
+
+request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=2')
+request.send()
