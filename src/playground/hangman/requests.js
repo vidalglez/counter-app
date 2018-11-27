284d1db8fc6dbd32b34552b6e840498479b4b705
Vidal Gonzalez <darkvidal@gmail.com>
2018-11-26 22:11:41 -0600

    feature: aysnc- await for location api

--- src/playground/hangman/requests.js
+++ src/playground/hangman/requests.js
@@ -1,39 +1,33 @@
-const getPuzzle = wordCount => {
-    return fetch(`https://puzzle.mead.io/puzzle?wordCount=${wordCount}`).then(response => {
-        if(response.status === 200){
-            return response.json()
-        } else {
-            throw new Error('Unable to fetch puzzle')
-        }
-    }).then(data => {
+const getPuzzle = async wordCount => {
+    const response = await fetch(`https://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+    if(response.status === 200){
+        const data = await response.json()
         return data.puzzle
-    })
+    } else {
+        throw new Error('Unable to fetch puzzle')
+    }
 }
 
-const getCountry = countryCode => {
-    return fetch('https://restcountries.eu/rest/v2/all').then(response => {        
-        if (response.status === 200){
-            return response.json()
-        } else {
-            throw new Error('Unable to fetch country info')
-        }
-    }).then(countries => {
-        return countries
-    }).then(countries => {
-        const country = countries.find(countryItem => countryItem.alpha2Code === countryCode)
+const getCountry = async countryCode => {
+    const response = await fetch('https://restcountries.eu/rest/v2/all')
+    if(response.status === 200){
+        let countries = await response.json()
+        const country = countries.find(itemCountry => itemCountry.alpha2Code === countryCode)
         return country.name
-    })
+    } else {
+        throw new Error('Unable to fetch country info')
+    }
 }
 
-const getLocation = () => fetch('https://ipinfo.io/json?token=1a11bd55cc8f9c').then(response => {
-    if(response.status === 200) {
-        return response.json()
+const getLocation = async () => {
+    const response = await fetch('https://ipinfo.io/json?token=1a11bd55cc8f9c')
+    if(response.status === 200){
+        const location = await response.json()
+        return location
     } else {
         throw new Error('Unable to fetch current location')
     }
-}).then((data) => {
-    return data
-})
+}
 
 getPuzzle(2).then(puzzle => {
     console.log(puzzle)
@@ -70,3 +64,16 @@ getLocation().then(location => {
 }).then( country => {
     console.log(`The country name through chained fetch is: ${country}`)
 }).catch(err => console.log(err))
+
+const getCurrentCountry = async () => {
+    const location = await getLocation()    
+    const currentCountry = await getCountry(location.country)    
+    return currentCountry
+}
+
+getCurrentCountry().then(country => {
+    console.log('getCurrentCountry result: ' + country)
+}).catch(err => {
+    console.log(err)
+})
+
